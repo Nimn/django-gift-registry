a9f6c7ec59e07f9240749f50b68537dcc76ff6d6
Étienne Loks <etienne@peacefrogs.net>
2019-12-08 11:59:56 +0100

    Fix url

--- main_site/gift_registry/urls.py
+++ main_site/gift_registry/urls.py
@@ -4,11 +4,11 @@ from gift_registry import views
 
 urlpatterns = [
     url(r'^$', views.home, name='gift_home'),
-    url(r'^([-a-z]+)/$', views.GiftListView.as_view(), name='gift_list'),
+    url(r'^([-a-z0-9]+)/$', views.GiftListView.as_view(), name='gift_list'),
     url(r'^gift/(\d+)/$', views.detail, name="gift_detail"),
     url(r'^gift/(\d+)/cancel/(.+)/$', views.cancel, name="gift_cancel"),
-    url(r'^([-a-z]+)/thanks-given/$', views.ThanksGiven.as_view(),
+    url(r'^([-a-z0-9]+)/thanks-given/$', views.ThanksGiven.as_view(),
         name='thanks_given'),
-    url(r'^([-a-z]+)/thanks-cancel/$', views.ThanksCancel.as_view(),
+    url(r'^([-a-z0-9]+)/thanks-cancel/$', views.ThanksCancel.as_view(),
         name='thanks_cancel'),
 ]
